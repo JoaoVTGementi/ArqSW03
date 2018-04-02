972c282e8a72a1d0fc06c9de27c33e7b5b57db72
JoaoVTGementi <36731139+JoaoVTGementi@users.noreply.github.com>
2018-04-02 20:28:09 -0300

    Add files via upload

---
+++ 81616650_Chamados_João_JPA/src/br/usjt/arqsw/service/LoginService.java
@@ -0,0 +1,26 @@
+package br.usjt.arqsw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.usjt.arqsw.dao.LoginDAO;
+
+/**
+ * 
+ * @author joão victor telles 81616650
+ *
+ */
+
+
+@Service
+public class LoginService {
+	private LoginDAO dao;
+	
+	@Autowired
+	public LoginService(LoginDAO dao){
+		this.dao = dao;
+	}
+
+}
+
+
